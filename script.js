9245d9779166555d0d066dfc48f423492a1c9ec8
Clarissa C <clarissa.colaco@hotmail.com>
2021-06-20 16:22:48 -0300

    movimentos da cobrinha setados

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "right";
+
+
+
+
 function criarBG(){
     context.fillStyle = "lightpink";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -25,9 +30,45 @@ function criarCobrinha(){
 
 
 
+//funcao q inicia as duas funcoes para o jogo comecar
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
 
-criarBG();
-criarCobrinha();
+    //posicao inicial
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    // plano cartesiano direita aumenta esq reduz
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    //up decre. down aumt.
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+
+    //funcao que tira ultimo elemento do array > funcao pop
+
+    snake.pop();
+
+    //criar nova cabeça da cobrinha - unshift
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+    
+
+
+
+}
+
+//intervalo tempo
+let jogo = setInterval(iniciarJogo, 100);
+
+
 
 
 
